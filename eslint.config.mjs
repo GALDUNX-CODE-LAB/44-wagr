d7cee2884dff17dd65e14c7b3fc9bf8b8fe5d1b0
sheriff10 <ibrahimsheriff999@gmail.com>
2025-08-08 12:07:51 +0100

    added fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "off", // disable globally
+      "@next/next/no-img-element": "off", // disable <img> warning
+    },
+  },
+];
 
 export default eslintConfig;
